80165a702aa63cd29dcd24716031d7d77b804efc
ZEUSOFCS <zeusofohio@gmail.com>
2016-11-07 23:39:08 -0500

    Update StudentInfo.c

--- StudentInfo.c
+++ StudentInfo.c
@@ -1,3 +1,13 @@
+
+/*
+ Author      : DORIAN JAVA BROWN
+ Version     : N/A
+ Copyright   : All Rights Reserve; You may use, distribute and modify this code.
+ Description : This program stores the information of 10 students using structures.
+ */
+
+
+
 #include <stdio.h>
 
 
