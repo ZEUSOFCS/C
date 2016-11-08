93c798bfe2589bfa6a2156aba3dea238379c77dc
ZEUSOFCS <zeusofohio@gmail.com>
2016-11-07 23:40:14 -0500

    Update StudentInfo.c

--- StudentInfo.c
+++ StudentInfo.c
@@ -3,7 +3,7 @@
  Author      : DORIAN JAVA BROWN
  Version     : N/A
  Copyright   : All Rights Reserve; You may use, distribute and modify this code.
- Description : This program stores the information of 10 students using structures.
+ Description : This program stores the information of 5 students using structures.
  */
 
 
@@ -16,7 +16,7 @@ struct student
   int ID;
   char firstName [20];
   char lastName [20];
-}
+} char studentNum[5];
 
 int main () 
 {
